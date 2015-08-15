82b5046db1a30512c5da6a4342035b82b7cb7e90
Vincent Kranendonk <kranendonk.vincent@gmail.com>
2015-08-15 14:29:27 +0200

    Rab-vslider is now rab-fast-scroll

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,3 @@
-// Gruntfile with the configuration of grunt-express and grunt-open. No livereload yet!
 module.exports = function(grunt) {
 
     // Load Grunt tasks declared in the package.json file
@@ -8,16 +7,12 @@ module.exports = function(grunt) {
     grunt.initConfig({
 
         // grunt-express will serve the files from the folders listed in `bases`
-        // on specified `port` and `hostname`
         express: {
             all: {
                 options: {
                     port: 9000,
                     hostname: "0.0.0.0",
-                    bases: [__dirname], // Replace with the directory you want the files served from
-                                        // Make sure you don't use `.` or `..` in the path as Express
-                                        // is likely to return 403 Forbidden responses if you do
-                                        // http://stackoverflow.com/questions/14594121/express-res-sendfile-throwing-forbidden-error
+                    bases: [__dirname],
                     livereload: false
                 }
             }
@@ -26,11 +21,6 @@ module.exports = function(grunt) {
         // grunt-watch will monitor the projects files
         watch: {
             all: {
-                // Replace with whatever file you want to trigger the update from
-                // Either as a String for a single entry
-                // or an Array of String for multiple entries
-                // You can use globing patterns like `css/**/*.css`
-                // See https://github.com/gruntjs/grunt-contrib-watch#files
                 files: 'index.html',
                 options: {
                     livereload: false
@@ -44,14 +34,30 @@ module.exports = function(grunt) {
                 // Gets the port from the connect configuration
                 path: 'http://localhost:<%= express.all.options.port%>/app'
             }
+        },
+
+        // build with vulcanize
+        vulcanize: {
+            default: {
+                options: {},
+                files: {
+                    'app/build.html': 'app/index.html'
+                }
+            }
         }
+
     });
 
     // Creates the `server` task
-    grunt.registerTask('server', [
+    grunt.registerTask('default', [
         'express',
         'open',
         'watch'
+    ]);
+
+    // Creates the `build` task
+    grunt.registerTask('build', [
+        'vulcanize'
     ]);
 
 };
